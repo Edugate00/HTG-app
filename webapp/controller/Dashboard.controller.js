e302b58c950b9d7cb2a032ff6e2bd9a3dcf22cfc
wildanGhifari <wildghifari@gmail.com>
2023-02-09 12:02:57 +0700

    feat : add Dialog fragment for Tagihan Air

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -5,7 +5,8 @@ sap.ui.define(
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/viz/ui5/data/DimensionDefinition",
-    "sap/ui/core/Item"
+    "sap/ui/core/Item",
+	"sap/ui/core/Fragment"
   ],
   function (
     BaseController,
@@ -13,7 +14,8 @@ sap.ui.define(
 	Filter,
 	FilterOperator,
 	DimensionDefinition,
-	Item
+	Item,
+	Fragment
   ) {
     "use strict";
 
@@ -72,7 +74,7 @@ sap.ui.define(
         });
 
         const tileTagihanAir = this.getView().byId("tileTagihanAir");
-        tileTagihanAir.attachBrowserEvent("click", this._onTagihanAirClick, false);
+        tileTagihanAir.attachBrowserEvent("click", this._onTagihanAirClick, this);
 
         const penggunaanAir = this.getOwnerComponent().getModel("penggunaanAir").getData();
         const penggunaanAirModel = new JSONModel(penggunaanAir);
@@ -82,8 +84,38 @@ sap.ui.define(
         oComboBoxTenant.fireSelectionChange();
       },
 
-      _onTagihanAirClick: function () {
-        console.log("WAODKOWAKDOA");
+      _onTagihanAirClick: function (oEvent) {
+        const oView = this.getView();
+        const oTagihanAirModel = [
+          {nama: "Tenant A", noTagihan: "90046015", dueDate: "09 Feb, 2023"},
+          {nama: "Tenant B", noTagihan: "90046016", dueDate: "09 Feb, 2023"},
+          {nama: "Tenant C", noTagihan: "90046017", dueDate: "09 Feb, 2023"},
+          {nama: "Tenant D", noTagihan: "90046018", dueDate: "09 Feb, 2023"},
+          {nama: "Tenant E", noTagihan: "90046019", dueDate: "09 Feb, 2023"}
+        ];
+
+        if (!this.tagihanAirDialog) {
+          this.tagihanAirDialog = Fragment.load({
+              id: oView.getId(),
+              name: "lrlpapp.view.fragments.TagihanAirDialog",
+              controller: this,
+          }).then(function (oDialog) {
+            oDialog.setModel(oView.getModel());
+            oDialog.setModel(new JSONModel({ 
+              tagihanAir: oTagihanAirModel
+            }), "tagihanAir");
+            return oDialog;
+          })
+        }
+
+        this.tagihanAirDialog.then(function(oDialog) {
+          oDialog.setModel(oView.getModel());
+            oDialog.setModel(new JSONModel({ 
+              tagihanAir: oTagihanAirModel
+            }), "tagihanAir");
+            
+            oDialog.open();
+        })
       },
 
       onTenantSelected: function (oEvent) {
@@ -141,6 +173,11 @@ sap.ui.define(
           oDataset.getBinding("data").filter(oFilterMonth);
         }
       },
+
+      // Dialogs Close
+      onTagihanAirDialogClose: function() {
+        this.byId("tagihanAirDialog").close()
+      }
     });
   }
 );
