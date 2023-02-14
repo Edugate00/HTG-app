2d819dfcdf774cc3129cf47e9f7d2276bc855a98
wildanGhifari <wildghifari@gmail.com>
2023-02-14 10:20:23 +0700

    fix(navbar) : remove maintenance

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -30,35 +30,30 @@ sap.ui.define(["lrlpapp/controller/BaseController"], function (BaseController) {
       const dashboardLink = this.getView().byId("dashbaord");
       const utilityLink = this.getView().byId("utility");
       const rentalLink = this.getView().byId("rental");
-      const maintenanceLink = this.getView().byId("maintenance");
 
       switch (page) {
         case "dashboard":
           dashboardLink.addStyleClass("nav-link-active");
           utilityLink.removeStyleClass("nav-link-active");
           rentalLink.removeStyleClass("nav-link-active");
-          maintenanceLink.removeStyleClass("nav-link-active");
           break;
 
         case "utility":
           dashboardLink.removeStyleClass("nav-link-active");
           utilityLink.addStyleClass("nav-link-active");
           rentalLink.removeStyleClass("nav-link-active");
-          maintenanceLink.removeStyleClass("nav-link-active");
           break;
 
         case "rental":
           dashboardLink.removeStyleClass("nav-link-active");
           utilityLink.removeStyleClass("nav-link-active");
           rentalLink.addStyleClass("nav-link-active");
-          maintenanceLink.removeStyleClass("nav-link-active");
           break;
 
         case "maintenance":
           dashboardLink.removeStyleClass("nav-link-active");
           utilityLink.removeStyleClass("nav-link-active");
           rentalLink.removeStyleClass("nav-link-active");
-          maintenanceLink.addStyleClass("nav-link-active");
           break;
       }
     },
