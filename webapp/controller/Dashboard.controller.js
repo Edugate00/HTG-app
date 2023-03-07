1100dbbdf608360ae2380e6fb4a8fda505455ae0
wildanGhifari <wildghifari@gmail.com>
2023-03-07 13:56:59 +0700

    refactor : clear console.log

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -154,13 +154,8 @@ sap.ui.define(
                 })
             })
         }
-
-        console.log(tagihanAir)
-
+        
         tagihanAirToCreate = scanned.length - tagihanAir.length;
-        console.log(scanned.length)
-        console.log(tagihanAir.length)
-        console.log(tagihanAirToCreate)
 
         BILLING_FV.forEach(el => {
             if (el.ReleasedStatus !== "X") {
